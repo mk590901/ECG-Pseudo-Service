8b8911541282c81b81269e2233895a6a7eb60a54
michael-k <michaelsure9@gmail.com>
2025-06-01 07:11:41 +0300

    Start with create/destroy simulators

--- lib/ui_components/card_view.dart
+++ lib/ui_components/card_view.dart
@@ -2,22 +2,32 @@
 import 'package:flutter/material.dart';
 
 import '../ui_blocks/item_model.dart';
+import '../widget/graph_widget.dart';
 
 class CardView extends StatelessWidget {
   final Item item;
+
+  //final GraphWidget graphWidget;
 
   const CardView({super.key, required this.item});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: ListTile(
-        leading: Icon(Icons.info_outline, color: Colors.lightBlue,),
-        title: Text(item.title),
-        subtitle: Text(item.subtitle, style: const TextStyle(
-            fontStyle: FontStyle.italic)),
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Card(
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: ListTile(
+            leading: Icon(Icons.info_outline, color: Colors.lightBlue),
+            title: Text(item.title),
+            subtitle: Text(
+              item.subtitle,
+              style: const TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
